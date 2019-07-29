9f73bb2b8d8d97e1af2fc5a6c9f51f4873af4d55
Piotrek Sroka <piotrek@ps-design.pl>
2019-07-29 19:07:06 +0200

    google check tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,9 @@
   <div>
     <app-header></app-header>
     <transition name="fade">
-    <nuxt/>
+      <nuxt />
     </transition>
-	<app-footer></app-footer>
+    <app-footer></app-footer>
   </div>
 </template>
 
@@ -12,74 +12,90 @@
 import AppHeader from "@/components/Nav/Header";
 import AppFooter from "@/components/Nav/Footer";
 export default {
-	components: {
-		AppHeader,
-		AppFooter
-	}
+  components: {
+    AppHeader,
+    AppFooter
+  },
+  head() {
+    return {
+      title: "Moje konto",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "google-site-verification",
+          content: "8SRdc8W60miQcIKkDS-T0Tp2Da8TanZdRh69k1R7Xo4"
+        }
+      ]
+    };
+  }
 };
 </script>
 
 
 <style>
 html {
-	font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-	font-size: 16px;
-	word-spacing: 1px;
-	-ms-text-size-adjust: 100%;
-	-webkit-text-size-adjust: 100%;
-	-moz-osx-font-smoothing: grayscale;
-	-webkit-font-smoothing: antialiased;
-	box-sizing: border-box;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
 }
 html,
 body {
-	width: 100%;
-	max-width: 1280px;
-	margin: 0 auto;
-	margin-bottom: 140px;
-	z-index: 1;
-	background-color: #ecf0f2;
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  margin-bottom: 140px;
+  z-index: 1;
+  background-color: #ecf0f2;
 }
 
 *,
 *:before,
 *:after {
-	box-sizing: border-box;
-	margin: 0;
+  box-sizing: border-box;
+  margin: 0;
 }
 
 .button--green {
-	display: inline-block;
-	border-radius: 4px;
-	border: 1px solid #3b8070;
-	color: #3b8070;
-	text-decoration: none;
-	padding: 10px 30px;
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
 }
 
 .button--green:hover {
-	color: #fff;
-	background-color: #3b8070;
+  color: #fff;
+  background-color: #3b8070;
 }
 
 .button--grey {
-	display: inline-block;
-	border-radius: 4px;
-	border: 1px solid #35495e;
-	color: #35495e;
-	text-decoration: none;
-	padding: 10px 30px;
-	margin-left: 15px;
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
 }
 
 .button--grey:hover {
-	color: #fff;
-	background-color: #35495e;
+  color: #fff;
+  background-color: #35495e;
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 }
 </style>
